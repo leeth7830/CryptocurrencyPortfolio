46bdc0d185d5c51d04b98c5a2238562f6f80c49c
Taehyon Lee <leeth7830@gmail.com>
2017-12-27 23:15:26 -0800

    Homescreen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -5,7 +5,7 @@ import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  HomeScreen: { screen: HomeScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
